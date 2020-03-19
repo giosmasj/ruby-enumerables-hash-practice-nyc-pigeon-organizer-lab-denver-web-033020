20ef7f57c8a5cbfe541c2fb4e08f3680f7b9bc1d
James Giosmas <giosmasj@gmail.com>
2020-03-19 18:48:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -55,7 +55,20 @@ pigeon_list = {
   }
 }
 
-
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
 
 def nyc_pigeon_organizer(data)
 hash = {}
